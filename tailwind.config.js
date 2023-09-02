a1af748a4757aee4b13958ba97014e5cc90faa0f
Cristian-DW <cristtiiank@gmail.com>
2023-09-01 19:15:22 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export const theme = {
     colors: {
       fondo: '#060918',
       fondo2: '#6919ff',
-      fondo3: '#943894',
+      fondo3: '#588157',
       fondo4: '#3f9499',
       letra: '#fffef9',
     },
