a6c4008c75e3e1b30ebf8ca5a85c375a7550d75f
Cristian-DW <cristtiiank@gmail.com>
2023-10-22 12:02:44 -0500

    changes text welcome (hero)

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Down from '../assets/down.svg'
-
+import { Link } from 'react-scroll';
 
 
 
@@ -20,16 +20,19 @@ function Hero() {
           Desarrollador Frontend
         </h1>
         <div className="  w-full  2xl:h-[20rem] flex justify-center  2xl:justify-between  items-center 2xl:items-stretch">
-          <button className="button  2xl:mt-28  2xl:w-72  2xl:ml-28 hover:text-fondo  animate-fade-down animate-once animate-delay-[1500ms] animate-ease-linear  ">
+        <Link
+                to="porfolio"
+                smooth={true}
+                duration={900}>
+          <button className="button  2xl:mt-28  2xl:w-72  2xl:ml-28 hover:text-fondo  animate-fade-down animate-once animate-delay-[3000ms] animate-ease-linear  ">
             <span className="relative z-10">
-              <a href="#porfolio" className=" text-sm 2xl:text-xl">Explora mis creaciones </a>
+                  <a href="#porfolio" className="text-sm xl:text-xl">Explora mis creaciones</a>
             </span>
           </button>
+           </Link>
           <p className=" hidden 2xl:block 2xl:mt-28 2xl:w-[35rem] 2xl:mb-44 2xl:text-lg text-end font-light">
             {" "}
-            Exploro el emocionante universo del desarrollo frontend con pasión y
-            creatividad, fusionando diseño y código para crear experiencias web
-            que cautivan y conectan
+            Bienvenido a mi mundo en el desarrollo frontend! Aquí, la pasión y la creatividad se unen en un abrazo apasionado. Mi misión es fusionar diseño y código para dar vida a experiencias web que no solo cautivan, ¡sino que también conectan!
           </p>
         </div>
         <a
