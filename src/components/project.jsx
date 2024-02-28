0bd9f234537096ac9c130b84fdd20e24d7c757c2
Cristian-DW <cristtiiank@gmail.com>
2024-02-28 17:14:17 -0500

    feat: :art: Add carousel to projects section
    Implement carousel functionality in the projects section  This commit introduces the integration of a carousel in the projects section of the Project component. A "keep-react" Carousel component was utilized to visually showcase projects in a carousel format.  Images from the projects array were used to populate the carousel slides, and options such as ring indicators and the display of three items at a time were configured. This significantly enhances the visual presentation of projects and allows users to navigate more efficiently between them.

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,16 +1,12 @@
-// eslint-disable-next-line no-unused-vars
+"use client";
 import React from 'react';
 import '../App.css';
+import { Carousel } from "keep-react";
 import ProjectUno from '../assets/projectUno.webp'
 import ProjectDos from '../assets/project2.webp'
 import ProjectTres from '../assets/project-3.webp'
 import ProjectCuatro from '../assets/Project4.png'
 import ProjectsCard from './extras/cards';
-
-
-
-
-// eslint-disable-next-line react/prop-types
 
 
 const Project = () => {
@@ -41,7 +37,7 @@ const Project = () => {
       link:"https://cristian-dw.github.io/todoList/" ,
     },
      {
-      title: "Weather App",
+      title: "Weather App"
       description: "App",
       fecha: "Julio de 2023",
       backgroundImage: ProjectCuatro,
@@ -51,10 +47,11 @@ const Project = () => {
   ];
 
   return (
-    <div id="porfolio"  className="flex flex-col  my-28 2xl:px-[10rem] justify-center items-center min-h-screen  ">
-      <h2 className="subtitle text-center">Proyectos</h2>
-      <p  className=" text-md font-light 2xl:text-lg text-center md:mb-12">Explora mis creaciones</p>
-      <div className="flex flex-wrap justify-center items-center  mx-auto gap-6 p-5 2xl:p-0 ">
+    <>
+    <h2 className='text-center mb-4 text-4xl 2xl:text-5xl'>Proyectos</h2>
+    <p className='text-center text-lg 2xl:text-xl'>Explora mi maravilloso mundo</p>
+    <Carousel indicatorsType="ring" indicators={true} itemToShow={3} autoPlay={false} className='w-full min-h-screen'>
+      <div className="flex flex-wrap justify-center items-center  mx-auto gap-12 p-5 rounded-xl 2xl:p-0  ">
         {projects.map((project, index) => (
           <ProjectsCard
             key={index}
@@ -64,11 +61,11 @@ const Project = () => {
             backgroundImage={project.backgroundImage}
             link={project.link}
             link2={project.link2}
-
           />
         ))}
       </div>
-    </div>
+      </Carousel>
+      </>
   );
 };
 
