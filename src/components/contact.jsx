12a5bc7585b88d5d6caa3a11d2132caee5e638d6
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-06-20 20:02:13 -0500

    feat: ✨ add ContactForm component with state management and form handling
    - Added ContactForm component to handle user contact submissions.
    - Implemented state management using useState hook for form data.
    - Created handleChange and handleSubmit functions to manage form input and submission.
    - Styled the component to ensure responsiveness and proper alignment.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,19 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-
-const ContactForm = () => {
+/**
+ * ContactForm component.
+ * This component displays a contact form allowing users to input their name, email, and a message.
+ * The form data is managed using the useState hook.
+ */
+const ContactForm: React.FC = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: ''
   });
 
-  const handleChange = (e) => {
+  /**
+   * Handles input changes and updates the formData state.
+   *
+   * @param {ChangeEvent<HTMLInputElement | HTMLTextAreaElement>} e - The change event object.
+   */
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  /**
+   * Handles form submission.
+   *
+   * @param {FormEvent<HTMLFormElement>} e - The form event object.
+   */
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Aquí puedes agregar el código para enviar el formulario a tu servidor o realizar alguna acción con los datos recopilados.
     console.log(formData);
@@ -22,28 +36,26 @@ const ContactForm = () => {
   return (
     <div
       id="contact"
-      className="px-10 md:px-40  lg:px-96 mb-24 py-10   2xl:grid  grid-cols-7 justify-center items-center 2xl:px-40 2xl:min-h-screen  "
+      className="px-10 md:px-40 lg:px-96 mb-24 py-10 2xl:grid grid-cols-7 justify-center items-center 2xl:px-40 2xl:min-h-screen"
     >
-      <div className="mb-8 2xl:col-span-3 2xl:col-start-2 flex-col  justify-center items 2xl:pr-32">
+      <div className="mb-8 2xl:col-span-3 2xl:col-start-2 flex-col justify-center items 2xl:pr-32">
         <h3 className="subtitle text-center 2xl:text-left">Contáctame</h3>
-        <h4 className="text   text-center 2xl:text-left ">
+        <h4 className="text text-center 2xl:text-left">
           <span className="text-xl 2xl:text-3xl">
             ¿Tienes un problema que resolver?
           </span>
         </h4>
-        <p className="text-md font-light text-center 2xl:text-left  2xl:text-2xl">
+        <p className="text-md font-light text-center 2xl:text-left 2xl:text-2xl">
           Cuéntame tus ideas para desarrollar el sitio web de tus sueños.
         </p>
       </div>
 
-      <div className="2xl:col-span-2 2xl:2xl:col-start-5 ">
+      <div className="2xl:col-span-2 2xl:2xl:col-start-5">
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col mb-4">
-            <label htmlFor="name" className="mb-1 font-light ">
-              Nombre
-            </label>
+            <label htmlFor="name" className="mb-1 font-light">Nombre</label>
             <input
-              className="w-full 2xl:w-full px-4 py-3 border border-gray-300  bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors  "
+              className="w-full 2xl:w-full px-4 py-3 border border-gray-300 bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors"
               type="text"
               id="name"
               name="name"
@@ -53,11 +65,9 @@ const ContactForm = () => {
             />
           </div>
           <div className="flex flex-col mb-4">
-            <label htmlFor="email" className="mb-1 font-light ">
-              Email
-            </label>
+            <label htmlFor="email" className="mb-1 font-light">Email</label>
             <input
-              className="w-full 2xl:w-full px-4 py-3 border border-gray-300  bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors "
+              className="w-full 2xl:w-full px-4 py-3 border border-gray-300 bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors"
               type="email"
               id="email"
               name="email"
@@ -67,11 +77,9 @@ const ContactForm = () => {
             />
           </div>
           <div className="flex flex-col mb-4">
-            <label htmlFor="message" className="mb-1 font-light  ">
-              Mensaje
-            </label>
+            <label htmlFor="message" className="mb-1 font-light">Mensaje</label>
             <textarea
-              className="w-full 2xl:w-full min-h-20 max-h-32   px-4 py-3 border border-gray-300  bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors "
+              className="w-full 2xl:w-full min-h-20 max-h-32 px-4 py-3 border border-gray-300 bg-transparent focus:ring-fondo2 focus:border-fondo2 focus:outline-none focus:ring-2 transition-colors"
               id="message"
               name="message"
               value={formData.message}
@@ -80,10 +88,10 @@ const ContactForm = () => {
             />
           </div>
           <button
-            className="flex mx-auto h-12 w-full md:w-[28.2rem] 2xl:w-full mt-10 items-center justify-center overflow-hidden bg-transparent border shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:bg-white  before:text-opacity-10 before:duration-500 before:ease-out hover:shadow-white hover:before:h-12 hover:before:w-[28.2rem] hover:text-fondo"
+            className="flex mx-auto h-12 w-full md:w-[28.2rem] 2xl:w-full mt-10 items-center justify-center overflow-hidden bg-transparent border shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:bg-white before:text-opacity-10 before:duration-500 before:ease-out hover:shadow-white hover:before:h-12 hover:before:w-[28.2rem] hover:text-fondo"
             type="submit"
           >
-            <span className="z-20">Enviar &#x1F680;</span>
+            <span className="z-20">Enviar 🚀</span>
           </button>
         </form>
       </div>
@@ -92,4 +100,3 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
-
