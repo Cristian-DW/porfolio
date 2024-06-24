97549f6b1bba6a9fd1d71d4339d690a78c9c3297
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-06-23 20:34:18 -0500

    feat: ✨ add dynamic Footer component with navigation links and dynamic background color
    - Implemented the Footer component with dynamic background color based on the selectedColor prop.
    - Added smooth scrolling navigation links to different sections of the page.
    - Included a logo and copyright notice with a link to the LinkedIn profile.
    - Styled the component for proper alignment and responsiveness.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,80 +4,55 @@ import React, { useEffect, useState } from 'react';
 import Logo from '../assets/logo.svg';
 import { Link } from 'react-scroll';
 
+/**
+ * Footer component.
+ * This component displays the footer with navigation links and dynamically adjusts the background color based on the selectedColor prop.
+ * @param {string} selectedColor - The selected background color class.
+ */
+const Footer: React.FC<{ selectedColor: string }> = ({ selectedColor }) => {
+  const [backgroundClass, setBackgroundClass] = useState('bg-fondo2');
 
-
-const Footer = ({ selectedColor }) => {
-    const [backgroundClass, setBackgroundClass] = useState('bg-fondo2');
-  
-    useEffect(() => {
-      // Cuando selectedColor cambia en el componente App, actualiza backgroundClass
-      if (selectedColor === 'bg-fondo3') {
+  useEffect(() => {
+    // Update backgroundClass when selectedColor changes
+    switch (selectedColor) {
+      case 'bg-fondo3':
         setBackgroundClass('bg-fondo3');
-      } else if (selectedColor === 'bg-fondo4') {
+        break;
+      case 'bg-fondo4':
         setBackgroundClass('bg-fondo4');
-      } else {
-        setBackgroundClass('bg-fondo2'); // Establece un valor predeterminado si es necesario
-      }
-    }, [selectedColor]);
-
+        break;
+      default:
+        setBackgroundClass('bg-fondo2'); // Default value
+        break;
+    }
+  }, [selectedColor]);
 
   return (
-    <div className={`${backgroundClass} bg-fondo2 h-[20rem] dark:bg-gray-900`}>
+    <div className={`${backgroundClass} h-[20rem] dark:bg-gray-900`}>
       <div className="w-full max-w-screen-xl mx-auto p-4 2xl:py-8">
-        <div className="flex items-center justify-center border-b mb-3 2xl:mb-16 ">
-          <a href="/" className="flex items-center  sm:mb-0">
-            <img src={Logo} className=" h-16 2xl:h-20" alt="Logo" />
+        <div className="flex items-center justify-center border-b mb-3 2xl:mb-16">
+          <a href="/" className="flex items-center sm:mb-0">
+            <img src={Logo} className="h-16 2xl:h-20" alt="Logo" />
           </a>
         </div>
-        <div className="flex items-center justify-center  2xl:mb-16">
-          <div className="flex space-x-3  2xl:space-x-10 text-white">
-            <Link
-              to="about"
-              smooth={true}
-              duration={900}
-              className="py-4 text-xs 2xl:text-lg md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-            >
-              Conóceme
-            </Link>
-            <Link
-              to="skill"
-              smooth={true}
-              duration={900}
-              offset={-420}
-              className="py-4 text-xs 2xl:text-lg md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-            >
-              Habilidades
-            </Link>
-            <Link
-              to="education"
-              smooth={true}
-              duration={900}
-              className=" py-4 text-xs 2xl:text-lg  md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-            >
-              Educación
-            </Link>
-            <Link
-              to="porfolio"
-              smooth={true}
-              duration={900}
-              offset={-100}
-              className="py-4 text-xs 2xl:text-lg md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-            >
-              Proyectos
-            </Link>
-
-            <Link
-              to="contact"
-              smooth={true}
-              duration={900}
-              className="py-4 text-xs 2xl:text-lg md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-            >
-              Contáctame
-            </Link>
+        <div className="flex items-center justify-center 2xl:mb-16">
+          <div className="flex space-x-3 2xl:space-x-10 text-white">
+            {['about', 'skill', 'education', 'porfolio', 'contact'].map((section, index) => (
+              <Link
+                key={index}
+                to={section}
+                smooth={true}
+                duration={900}
+                offset={section === 'skill' ? -420 : section === 'porfolio' ? -100 : 0}
+                className="py-4 text-xs 2xl:text-lg md:font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
+              >
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </Link>
+            ))}
           </div>
         </div>
-        <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700  mb-12  2xl:mb-4 " />
-        <span className="block text-sm  2xl:text-lg text-letra text-center dark:text-gray-400">
+        <hr className="border-gray-200 sm:mx-auto dark:border-gray-700 mb-12 2xl:mb-4" />
+        <span className="block text-sm 2xl:text-lg text-letra text-center dark:text-gray-400">
           © 2023{" "}
           <a
             href="https://www.linkedin.com/in/cristian-castro-pineda/"
