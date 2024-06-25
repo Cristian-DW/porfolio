6322f2f6ffbb1767b59fdbbed8208c501092d563
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-06-24 20:34:22 -0500

    feat: ✨ add responsive navigation bar with mobile menu functionality
    - Implemented the Nav component to provide a navigation bar with links to different sections of the page.
    - Added a responsive design that includes a mobile menu.
    - Included functionality to toggle the mobile menu and disable page scroll when the menu is open.
    - Applied animations for a smooth and engaging user experience.
    - Ensured accessibility by managing ARIA attributes for the mobile menu.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,13 @@
-
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import Logo from '../assets/logo.svg';
-import Menu from '../assets/menu.svg'
+import Menu from '../assets/menu.svg';
 import { Link } from 'react-scroll';
 
+/**
+ * Nav component.
+ * This component provides the navigation bar with links to different sections of the page.
+ * It includes a responsive design with a mobile menu.
+ */
 const Nav = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -15,34 +18,30 @@ const Nav = () => {
   const mobileMenuClasses = isMobileMenuOpen ? 'block' : 'hidden';
 
   useEffect(() => {
-    // Deshabilitar scroll cuando se abre el menú
-    if (isMobileMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
+    // Disable scroll when the mobile menu is open
+    document.body.style.overflow = isMobileMenuOpen ? 'hidden' : 'auto';
   }, [isMobileMenuOpen]);
 
   return (
     <>
       <nav className="fixed top-0 left-0 z-30 w-full border-b-2 backdrop-blur-lg animate-fade-down animate-once animate-ease-linear">
-        <div className="mx-auto max-w-8xl px-2  2xl:px-36">
+        <div className="mx-auto max-w-8xl px-2 2xl:px-36">
           <div className="flex h-16 items-center justify-between">
             <div className="absolute t-0 right-2 flex items-center 2xl:hidden">
               <button
                 type="button"
-                className="relative inline-flex items-center justify-center  hover:scale-125"
+                className="relative inline-flex items-center justify-center hover:scale-125"
                 onClick={toggleMobileMenu}
                 aria-controls="mobile-menu"
                 aria-expanded={isMobileMenuOpen}
               >
-                  <img width="30px" height="30px" src={Menu} alt="menu"/>
+                <img width="30px" height="30px" src={Menu} alt="menu"/>
               </button>
             </div>
             <div className="flex flex-1 items-center 2xl:items-stretch 2xl:justify-between">
               <div className="flex flex-shrink-0 items-end space-x-4 2xl:mr-20">
                 <a className="button-hover" href="/">
-                  <img  width="12px" height="12px"  className="h-12 w-auto" src={Logo} alt="logo" />
+                  <img width="12px" height="12px" className="h-12 w-auto" src={Logo} alt="logo" />
                 </a>
                 <a href="/" className="py-2 font-nav font-medium button-hover 2xl:block">
                   Cristian Castro
@@ -50,45 +49,21 @@ const Nav = () => {
               </div>
               <div className="hidden 2xl:ml-6 2xl:block">
                 <div className="flex space-x-8 text-white">
-                <Link
-                  to="about"
-                  smooth={true}
-                  duration={900}
-                  className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
+                  <Link to="about" smooth={true} duration={900} className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
                     Conóceme
                   </Link>
-                  <Link
-                  to="skill"
-                  smooth={true}
-                  duration={900}
-                  offset={-420} 
-                  className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
+                  <Link to="skill" smooth={true} duration={900} offset={-420} className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
                     Habilidades
                   </Link>
-                  <Link
-                      to="education"
-                      smooth={true}
-                      duration={900}
-                      className="px-2 py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer"
-                    >
+                  <Link to="education" smooth={true} duration={900} className="px-2 py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
                     Educación
-                    </Link>
-                    <Link
-                  to="porfolio"
-                  smooth={true}
-                  duration={900}
-                  offset={-100} 
-                  className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
+                  </Link>
+                  <Link to="porfolio" smooth={true} duration={900} offset={-100} className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
                     Proyectos
-                    </Link>
-                    
-                    <Link
-                  to="contact"
-                  smooth={true}
-                  duration={900}
-                  className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
+                  </Link>
+                  <Link to="contact" smooth={true} duration={900} className="py-2 font-nav font-medium hover:border-b-2 button-hover cursor-pointer">
                     Contáctame
-                    </Link>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -97,54 +72,27 @@ const Nav = () => {
       </nav>
 
       {/* Capa de fondo desenfocado */}
-      {isMobileMenuOpen && <div className="fixed inset-0 top-16 right-0 w-full z-30 backdrop-blur-lg backdrop-filter animate-fade-left animate-once animate-duration-500 animate-delay-100 animate-ease-linear   " onClick={toggleMobileMenu} />}
+      {isMobileMenuOpen && <div className="fixed inset-0 top-16 right-0 w-full z-30 backdrop-blur-lg backdrop-filter animate-fade-left animate-once animate-duration-500 animate-delay-100 animate-ease-linear" onClick={toggleMobileMenu} />}
 
       {/* Componente del menú desplegable */}
       {isMobileMenuOpen && (
-        <div
-          className={`2xl:hidden ${mobileMenuClasses} fixed top-16 right-0 w-full h-full bg-fondo z-30 animate-fade-left animate-once animate-duration-800 animate-delay-100 animate-ease-linear `}
-        >
+        <div className={`2xl:hidden ${mobileMenuClasses} fixed top-16 right-0 w-full h-full bg-fondo z-30 animate-fade-left animate-once animate-duration-800 animate-delay-100 animate-ease-linear`}>
           <div className="flex flex-col items-center justify-around w-full">
-            <Link
-                  to="about"
-                  smooth={true}
-                  duration={900}
-                  className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
-                    Conóceme
-                  </Link>
-                  <Link
-                  to="skill"
-                  smooth={true}
-                  duration={900}
-                  offset={-420} 
-                  className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
-                    Habilidades
-                  </Link>
-                  <Link
-                      to="education"
-                      smooth={true}
-                      duration={900}
-                      offset={-120} 
-                      className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
-                    
-                    Educación
-                    </Link>
-                    <Link
-                  to="porfolio"
-                  smooth={true}
-                  duration={900}
-                  offset={-100} 
-                  className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
-                    Proyecto
-                    </Link>
-            <Link
-                  to="contact"
-                  smooth={true}
-                  duration={900}
-                  offset={-100} 
-                  className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
-                    Contáctame
-                    </Link>
+            <Link to="about" smooth={true} duration={900} className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
+              Conóceme
+            </Link>
+            <Link to="skill" smooth={true} duration={900} offset={-420} className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
+              Habilidades
+            </Link>
+            <Link to="education" smooth={true} duration={900} offset={-120} className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
+              Educación
+            </Link>
+            <Link to="porfolio" smooth={true} duration={900} offset={-100} className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
+              Proyecto
+            </Link>
+            <Link to="contact" smooth={true} duration={900} offset={-100} className="block py-14 text-lg w-full text-center cursor-pointer" onClick={toggleMobileMenu}>
+              Contáctame
+            </Link>
           </div>
         </div>
       )}
@@ -153,5 +101,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-
