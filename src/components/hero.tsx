0deb8fd1930f80a0a6b1888d09b6e6566a8a30f7
Cristian-CCP <ccacastrop@poligran.edu.co>
2025-09-06 11:18:14 -0500

    docs: :recycle: refresh dependencies and adjust config settings
    update project dependencies and refine configuration settings for improved stability

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import Down from '../assets/down.svg';
+import Down from '../assets/down.svg'; 
 import { Link } from 'react-scroll';
 
 /**
